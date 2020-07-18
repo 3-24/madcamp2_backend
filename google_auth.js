3ae91dc6f9c4fed98933275ada42e4d50b5444e3
3-24 <dev.youngseok@gmail.com>
2020-07-18 18:32:51 +0000

    Handle signup query

--- google_auth.js
+++ google_auth.js
@@ -3,7 +3,6 @@ module.exports = async function(body){
 	const {auth} = require('google-auth-library');
 	const client = auth.fromAPIKey('AIzaSyAWywGxHc-GZ8-DhduNcOnMj4cTEktwStU');
 	const res = await client.verifyIdToken({idToken});
-	
 	const {email, name, picture, sub: googleid} = res.getPayload();
-	console.log(email);
+	return email;
 }
--- login.js
+++ login.js
@@ -5,6 +5,17 @@ var path = require('path')
 var app = express();
 var google_auth = require('./google_auth');
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'nodelogin'
+});
+
+connection.connect();
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
@@ -14,15 +25,46 @@ app.use(function(req,res,next){
 	next();
 });
 
-
-app.post('/google_signin', function(req,res) {
-	console.log(req.body.idToken);
-	google_auth(req.body);
+app.post('/anon_signup', function(req,res) {
+	var email = req.body.email;
+	var password = req.body.password;
+	connection.query('SELECT * FROM accounts WHERE email=?', [email],
+		function(error, results, fields){
+			if (error){
+				res.send({"code": 400, "failed": "error occurred"});
+			} else {
+				if (results.length > 0){
+					res.send({"code": 401, "failed":"already exists"});
+				} else {
+					connection.query('INSERT INTO accounts SET ?',
+					{"email":email,"password":password}, function(_error, _results, _fields){
+						if (_error){
+							res.send({"code": 402, "failed": "error occurred"});
+						} else {
+							res.send({"code":200, "success": "anon_signup success"});
+						}
+					});
+				}
+			}
+		}
+	);
 });
 
+app.post('/google_signup', function(req,res) {
+	console.log(req.body.idToken);
+	var email = google_auth(req.body);
+	console.log(email);
+});
 
+app.post('/google_signin', function(req,res) {
+	//console.log(req.body.idToken);
+	var email = google_auth(req.body);
+});
 
-//app.use('api', router);
+app.get('/', function(req,res){
+	res.json({'message':'Welcome to madcamp2 backend server!'});
+});
+
 app.listen(80, "0.0.0.0", function(){
 	console.log("Express server has started on port 80");
 });
